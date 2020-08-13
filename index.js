4632da9eb28f751b5e835b2e5d866cce856854e2
the anh <you@example.com>
2020-08-13 10:35:22 +0700

    cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //-------------------------------khai báo------------------------------------------
 var controller= require('./controllers/user.controller')
 const express = require('express'); // Require module express vào project
+var cookieParser = require('cookie-parser')//cookie
 const app = express(); // Tạo một app mới
 //const shortid = require('shortid');
 const port = 3000; // Định nghĩa cổng để chạy ứng dụng NodeJS của bạn trên server
@@ -10,6 +11,7 @@ app.set('view engine', 'pug'); // Sử dụng pug làm view engine
 //---------------------------config của req.body ----------------------------------
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())//coookie
 //--------------------khởi tạo một database đơn giản ---------------
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
@@ -48,17 +50,29 @@ app.get('/users/search', (req,res) => {
 		users: result // render lại trang users/index với biến users bây giờ chỉ bao gồm các kết quả phù hợp
 	});
 })
-
+//-------------------------------cookie-------------------------------------
+app.get('/users/cookie',(req,res)=>{
+	res.cookie('name','test');
+	res.send('hello world');
+})
 // //----------------------------phương thức post-------------------------------
 app.get('/users/create',(req,res)=>{
+	//console.log(req.cookies); đọc cookie đã được nhận
 	res.render('users/create');  
 })
 
-app.post('/users/create', (req, res) => {
+app.post('/users/create', (req, res, next) => {
 	// req.body.id= shortid.generate();
+	//xác thực người dùng 
 	var error=[];  
 	if(!req.body.name){
 		error.push('khong co name');
+	}
+	if(!req.body.email){
+		error.push('khong co email');
+	}
+	if(!req.body.age){
+		error.push('khong co tuoi');
 	}
 	if(error.length){
 		res.render('users/create',{
@@ -67,9 +81,13 @@ app.post('/users/create', (req, res) => {
 		})
 		return;
 	} 
-   db.get('users').push(req.body).write()//thêm phần tử vào database 
-   res.redirect('/users')//chuyển hướng trở lại trang trước 
-})
+	next();//chuyển tới hàm tiếp theo 
+	},(req, res) => {
+		db.get('users').push(req.body).write()//thêm phần tử vào database 
+   		res.redirect('/users')//chuyển hướng trở lại trang trước 
+	}
+  
+)
 //----------------------hiển thị chi tiết --------------
 app.get('/users/:id',(req,res)=>{
 	var id = parseInt(req.params.id);
